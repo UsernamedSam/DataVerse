bc1724695ada69415e6c8211e943398c9290d4cf
userNamedSam <subiakarensamira@gmail.com>
2024-07-30 12:14:21 -0500

    main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js'; 
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
+const mainContainer = document.getElementById("root")
+mainContainer.appendChild(renderItems(data))
+console.log()
 
-console.log(example, renderItems(data), data);
